c6197971738ab8b2e74f17c078133c9a33b6f13a
ricknigel <35053403+ricknigel@users.noreply.github.com>
2022-10-02 18:57:44 +0900

    fixed error-report function

--- src/functions/error-report/main.py
+++ src/functions/error-report/main.py
@@ -1,6 +1,9 @@
+import base64
 import os
 from typing import TypedDict
+from zoneinfo import ZoneInfo
 import requests
+from datetime import datetime
 
 
 class Attribute(TypedDict):
@@ -14,33 +17,82 @@ class PubSubMessage(TypedDict):
     attributes: Attribute
 
 
+# slackのwebhookURL
 webhook_url = os.getenv('SLACK_WEBHOOK_URL')
 
 
 def handler(event: PubSubMessage, context):
-    print(event)
+    """
+    エンドポイント
+    """
     try:
         report_error(event)
     except Exception as e:
         # ここで何かしらエラーが起こった場合は、ループが起きないよう通知せず、ログに出力するのみとする。
-        post_slack_message("-", "error-report", "", e)
+        now = datetime.now(ZoneInfo("Asia/Tokyo"))
+        post_slack_message("-", "error-report", now, e)
         raise e
 
 
 def report_error(event: PubSubMessage):
-    error = event['data']
+    """
+    エラー内容をslackで通知します
+    """
+    error = base64.b64decode(event['data']).decode('utf-8')
     projectId = event['attributes']['projectId']
     functionName = event['attributes']['functionName']
-    eventTime = event['attributes']['eventTime']
+    unixtime = event['attributes']['eventTime']
 
+    eventTime = datetime.fromtimestamp(unixtime)
     print(f"functionName: {functionName}, Time: {eventTime}, error: {error}")
 
+    # slackへ通知
     post_slack_message(projectId, functionName, eventTime, error)
 
 
-def post_slack_message(projectId, functionName, eventTime, error):
-    blocks = []
+def post_slack_message(
+    projectId: str, functionName: str, eventTime: datetime, error: str
+):
+    blocks = [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": ":warning:  Cause Error  :warning:"
+            }
+        },
+        {
+            "type": "section",
+            "fields": [
+                {
+                    "type": "mrkdwn",
+                    "text": f"*Project:*\n{projectId}"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": f"*Function:*\n{functionName}"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": f"*EventTime:*\n{eventTime}"
+                },
+            ]
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*Error Log:*\n```{error}```"
+            }
+        },
+    ]
     requestBody = {
         "blocks": blocks
     }
-    requests.post(webhook_url, json=requestBody)
+    # slackへpostする
+    response = requests.post(webhook_url, json=requestBody)
+
+    if response.status_code != 200:
+        error_msg = f"Slack Webhook Error \
+            [Http Status: {response.status_code}], [Result {response.json()}]"
+        raise Exception(error_msg)
